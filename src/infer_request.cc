42fd219a75ca59863fc693693d6f656fa3c5f0c8
Iman Tabrizian <itabrizian@nvidia.com>
2021-08-02 20:34:22 -0400

    Add SaveToSharedMemory and LoadFromSharedMemory to InferRequest and InferResponse objects (#67)
    * Move InferenceRequest implementation to C++
    
    * Move InferResponse to c++
    
    * Add save to shared memory methods for pb_tensor
    
    * Disable copy and assignment constructors for PbTensors
    
    * Refactor input tensor creation on the main process
    
    * Refactor tensor creation on the main process
    
    * Add LoadFromShared memory to infer request
    
    * Add LoadFromSharedMemory to infer_response
    
    * Add BLS exec
    
    * Fix identity tensor
    
    * Fix type bytes tensor
    
    * Remove BLS related changes
    
    * Fix the ifdef flags for CPU only build
    
    * Review edits
    
    * Rebase fix

---
+++ src/infer_request.cc
@@ -0,0 +1,151 @@
+// Copyright 2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "infer_request.h"
+
+#ifdef TRITON_PB_STUB
+#include "infer_response.h"
+#include "pb_stub.h"
+#endif
+
+namespace triton { namespace backend { namespace python {
+
+InferRequest::InferRequest(
+    const std::string& request_id, uint64_t correlation_id,
+    const std::vector<std::shared_ptr<PbTensor>>& inputs,
+    const std::vector<std::string>& requested_output_names,
+    const std::string& model_name, const int64_t model_version)
+    : request_id_(request_id), correlation_id_(correlation_id), inputs_(inputs),
+      requested_output_names_(requested_output_names), model_name_(model_name),
+      model_version_(model_version)
+{
+}
+
+const std::vector<std::shared_ptr<PbTensor>>&
+InferRequest::Inputs()
+{
+  return inputs_;
+}
+
+const std::string&
+InferRequest::RequestId()
+{
+  return request_id_;
+}
+
+uint64_t
+InferRequest::CorrelationId()
+{
+  return correlation_id_;
+}
+
+const std::vector<std::string>&
+InferRequest::RequestedOutputNames()
+{
+  return requested_output_names_;
+}
+
+const std::string&
+InferRequest::ModelName()
+{
+  return model_name_;
+}
+
+int64_t
+InferRequest::ModelVersion()
+{
+  return model_version_;
+}
+
+void
+InferRequest::SaveToSharedMemory(
+    std::unique_ptr<SharedMemory>& shm_pool, Request* request_shm)
+{
+  request_shm->correlation_id = this->CorrelationId();
+  off_t id_offset;
+  SaveStringToSharedMemory(shm_pool, id_offset, this->RequestId().c_str());
+  request_shm->id = id_offset;
+  request_shm->requested_output_count = this->RequestedOutputNames().size();
+  off_t requested_output_names_offset;
+  off_t* requested_output_names;
+  shm_pool->Map(
+      (char**)&requested_output_names,
+      sizeof(off_t) * request_shm->requested_output_count,
+      requested_output_names_offset);
+
+  request_shm->requested_output_names = requested_output_names_offset;
+  size_t i = 0;
+  for (auto& requested_output_name : requested_output_names_) {
+    SaveStringToSharedMemory(
+        shm_pool, requested_output_names[i], requested_output_name.c_str());
+    i++;
+  }
+
+  request_shm->requested_input_count = this->Inputs().size();
+  request_shm->model_version = this->model_version_;
+  SaveStringToSharedMemory(
+      shm_pool, request_shm->model_name, this->model_name_.c_str());
+}
+
+std::unique_ptr<InferRequest>
+InferRequest::LoadFromSharedMemory(
+    std::unique_ptr<SharedMemory>& shm_pool, off_t request_offset)
+{
+  Request* request;
+  shm_pool->MapOffset((char**)&request, request_offset);
+
+  char* id = nullptr;
+  LoadStringFromSharedMemory(shm_pool, request->id, id);
+
+  uint32_t requested_input_count = request->requested_input_count;
+
+  std::vector<std::shared_ptr<PbTensor>> py_input_tensors;
+  for (size_t input_idx = 0; input_idx < requested_input_count; ++input_idx) {
+    std::shared_ptr<PbTensor> pb_input_tensor = PbTensor::LoadFromSharedMemory(
+        shm_pool, request->inputs + sizeof(Tensor) * input_idx);
+    py_input_tensors.emplace_back(std::move(pb_input_tensor));
+  }
+
+  std::vector<std::string> requested_output_names;
+  uint32_t requested_output_count = request->requested_output_count;
+  off_t* output_names;
+  shm_pool->MapOffset((char**)&output_names, request->requested_output_names);
+
+  for (size_t output_idx = 0; output_idx < requested_output_count;
+       ++output_idx) {
+    char* output_name = nullptr;
+    LoadStringFromSharedMemory(shm_pool, output_names[output_idx], output_name);
+    requested_output_names.emplace_back(output_name);
+  }
+
+  char* model_name;
+  LoadStringFromSharedMemory(shm_pool, request->model_name, model_name);
+  return std::make_unique<InferRequest>(
+      id, request->correlation_id, std::move(py_input_tensors),
+      requested_output_names, model_name, request->model_version);
+}
+
+}}}  // namespace triton::backend::python
--- src/pb_tensor.cc
+++ src/pb_tensor.cc
@@ -24,13 +24,21 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#ifdef TRITON_ENABLE_GPU
+#include <cuda.h>
+#endif
+
+#ifdef TRITON_PB_STUB
 #include "pb_stub_utils.h"
+namespace py = pybind11;
+#endif
 #include "pb_tensor.h"
 #include "pb_utils.h"
 
-namespace py = pybind11;
 
 namespace triton { namespace backend { namespace python {
+
+#ifdef TRITON_PB_STUB
 PbTensor::PbTensor(const std::string& name, py::object numpy_array)
     : name_(name)
 {
@@ -47,7 +55,18 @@ PbTensor::PbTensor(const std::string& name, py::object numpy_array)
     numpy_array = numpy.attr("ascontiguousarray")(numpy_array);
   }
   numpy_array_ = numpy_array;
-  memory_ptr_ = numpy_array_.request().ptr;
+
+  if (dtype_ == TRITONSERVER_TYPE_BYTES) {
+    py::module triton_pb_utils =
+        py::module::import("triton_python_backend_utils");
+    numpy_array_serialized_ =
+        triton_pb_utils.attr("serialize_byte_tensor")(numpy_array);
+    memory_ptr_ = numpy_array_serialized_.request().ptr;
+    byte_size_ = numpy_array_serialized_.nbytes();
+  } else {
+    memory_ptr_ = numpy_array_.request().ptr;
+    byte_size_ = numpy_array_.nbytes();
+  }
 
   // Initialize tensor dimension
   size_t dims_count = numpy_array_.ndim();
@@ -56,7 +75,6 @@ PbTensor::PbTensor(const std::string& name, py::object numpy_array)
   for (size_t i = 0; i < dims_count; i++) {
     dims_.push_back(numpy_shape[i]);
   }
-  byte_size_ = numpy_array_.nbytes();
 }
 
 PbTensor::PbTensor(
@@ -74,9 +92,21 @@ PbTensor::PbTensor(
     numpy_array = numpy.attr("ascontiguousarray")(numpy_array);
   }
   numpy_array_ = numpy_array;
+
+  if (dtype == TRITONSERVER_TYPE_BYTES) {
+    py::module triton_pb_utils =
+        py::module::import("triton_python_backend_utils");
+    numpy_array_serialized_ =
+        triton_pb_utils.attr("serialize_byte_tensor")(numpy_array);
+    memory_ptr_ = numpy_array_serialized_.request().ptr;
+    byte_size_ = numpy_array_serialized_.nbytes();
+
+  } else {
+    memory_ptr_ = numpy_array_.request().ptr;
+    byte_size_ = numpy_array_.nbytes();
+  }
   tensor_type_ = PYTHONBACKEND_NUMPY;
   memory_type_ = TRITONSERVER_MEMORY_CPU;
-  memory_ptr_ = numpy_array_.request().ptr;
   dtype_ = dtype;
 
   // Initialize tensor dimension
@@ -86,16 +116,16 @@ PbTensor::PbTensor(
   for (size_t i = 0; i < dims_count; i++) {
     dims_.push_back(numpy_shape[i]);
   }
-  byte_size_ = numpy_array_.nbytes();
   memory_type_id_ = 0;
   dl_managed_tensor_ = nullptr;
 }
+#endif // TRITON_PB_STUB
 
 PbTensor::PbTensor(
     const std::string& name, const std::vector<int64_t>& dims,
     TRITONSERVER_DataType dtype, TRITONSERVER_MemoryType memory_type,
     int64_t memory_type_id, void* memory_ptr, uint64_t byte_size,
-    DLManagedTensor* dl_managed_tensor)
+    DLManagedTensor* dl_managed_tensor, off_t shm_offset)
 {
   name_ = name;
   memory_ptr_ = memory_ptr;
@@ -103,14 +133,30 @@ PbTensor::PbTensor(
   memory_type_id_ = memory_type_id;
   dtype_ = dtype;
   dims_ = dims;
+  shm_offset_ = shm_offset;
+
+#ifdef TRITON_PB_STUB
   if (memory_type_ == TRITONSERVER_MEMORY_CPU ||
       memory_type_ == TRITONSERVER_MEMORY_CPU_PINNED) {
-    py::object numpy_array =
-        py::array(triton_to_pybind_dtype(dtype_), dims_, (void*)memory_ptr_);
-    numpy_array_ = numpy_array.attr("view")(triton_to_numpy_type(dtype_));
+    if (dtype != TRITONSERVER_TYPE_BYTES) {
+      py::object numpy_array =
+          py::array(triton_to_pybind_dtype(dtype_), dims_, (void*)memory_ptr_);
+      numpy_array_ = numpy_array.attr("view")(triton_to_numpy_type(dtype_));
+    } else {
+      py::object numpy_array = py::array(
+          triton_to_pybind_dtype(TRITONSERVER_TYPE_UINT8), {byte_size},
+          (void*)memory_ptr_);
+      py::module triton_pb_utils =
+          py::module::import("triton_python_backend_utils");
+      numpy_array_ =
+          triton_pb_utils.attr("deserialize_bytes_tensor")(numpy_array)
+              .attr("reshape")(dims);
+    }
   } else {
     numpy_array_ = py::none();
   }
+#endif
+
   byte_size_ = byte_size;
   dl_managed_tensor_ = dl_managed_tensor;
 
@@ -122,7 +168,7 @@ PbTensor::PbTensor(
 }
 
 bool
-PbTensor::IsCPU()
+PbTensor::IsCPU() const
 {
   if (tensor_type_ == PYTHONBACKEND_NUMPY ||
       ((tensor_type_ == PYTHONBACKEND_RAW ||
@@ -136,35 +182,36 @@ PbTensor::IsCPU()
 }
 
 TRITONSERVER_MemoryType
-PbTensor::MemoryType()
+PbTensor::MemoryType() const
 {
   return memory_type_;
 }
 
 int64_t
-PbTensor::MemoryTypeId()
+PbTensor::MemoryTypeId() const
 {
   return memory_type_id_;
 }
 
 uint64_t
-PbTensor::ByteSize()
+PbTensor::ByteSize() const
 {
   return byte_size_;
 }
 
 const std::vector<int64_t>&
-PbTensor::Dims()
+PbTensor::Dims() const
 {
   return dims_;
 }
 
 PYTHONBACKEND_TensorType
-PbTensor::TensorType()
+PbTensor::TensorType() const
 {
   return tensor_type_;
 }
 
+#ifdef TRITON_PB_STUB
 void
 delete_unused_dltensor(PyObject* dlp)
 {
@@ -175,9 +222,20 @@ delete_unused_dltensor(PyObject* dlp)
   }
 }
 
+std::shared_ptr<PbTensor>
+PbTensor::FromNumpy(const std::string& name, py::object numpy_array)
+{
+  return std::make_shared<PbTensor>(name, numpy_array);
+}
+
 py::capsule
 PbTensor::ToDLPack()
 {
+  if (dtype_ == TRITONSERVER_TYPE_BYTES) {
+    throw PythonBackendException(
+        "DLPack does not have support for string tensors.");
+  }
+
   DLManagedTensor* dlpack_tensor = new DLManagedTensor;
   dlpack_tensor->dl_tensor.ndim = dims_.size();
   dlpack_tensor->dl_tensor.byte_offset = 0;
@@ -187,11 +245,6 @@ PbTensor::ToDLPack()
   dlpack_tensor->deleter = [](DLManagedTensor* m) {};
   dlpack_tensor->dl_tensor.device.device_id = memory_type_id_;
   dlpack_tensor->dl_tensor.dtype = triton_to_dlpack_type(dtype_);
-
-  if (dtype_ == TRITONSERVER_TYPE_BYTES) {
-    throw PythonBackendException(
-        "DLPack does not have support for string tensors.");
-  }
 
   switch (memory_type_) {
     case TRITONSERVER_MEMORY_GPU:
@@ -208,6 +261,7 @@ PbTensor::ToDLPack()
   return py::capsule(
       static_cast<void*>(dlpack_tensor), "dltensor", &delete_unused_dltensor);
 }
+#endif // TRITON_PB_STUB
 
 void
 PbTensor::DeleteDLPack()
@@ -218,7 +272,75 @@ PbTensor::DeleteDLPack()
   }
 }
 
-std::unique_ptr<PbTensor>
+std::shared_ptr<PbTensor>
+PbTensor::LoadFromSharedMemory(
+    std::unique_ptr<SharedMemory>& shm_pool, off_t tensor_offset)
+{
+  Tensor* tensor_shm;
+  shm_pool->MapOffset((char**)&tensor_shm, tensor_offset);
+
+  char* name;
+  LoadStringFromSharedMemory(shm_pool, tensor_shm->name, name);
+  std::string name_str = name;
+
+  size_t dims_count = tensor_shm->dims_count;
+  RawData* raw_data;
+  shm_pool->MapOffset((char**)&raw_data, tensor_shm->raw_data);
+
+  int64_t* dims;
+  shm_pool->MapOffset((char**)&dims, tensor_shm->dims);
+
+  std::string reused_gpu_tensor_name;
+  std::shared_ptr<PbTensor> pb_tensor;
+
+  char* data = nullptr;
+  if (raw_data->memory_type == TRITONSERVER_MEMORY_CPU) {
+    shm_pool->MapOffset((char**)&data, raw_data->memory_ptr);
+    pb_tensor = std::make_shared<PbTensor>(
+        name, std::vector<int64_t>(dims, dims + dims_count), tensor_shm->dtype,
+        raw_data->memory_type, raw_data->memory_type_id, data,
+        raw_data->byte_size, nullptr /* DLManaged Tensor */);
+  } else if (raw_data->memory_type == TRITONSERVER_MEMORY_GPU) {
+#ifdef TRITON_ENABLE_GPU
+    cudaIpcMemHandle_t* cuda_ipc_mem_handle;
+    shm_pool->MapOffset((char**)&cuda_ipc_mem_handle, raw_data->memory_ptr);
+    if (!tensor_shm->is_reused) {
+      cudaSetDevice(raw_data->memory_type_id);
+
+      cudaError_t err = cudaIpcOpenMemHandle(
+          (void**)&data, *cuda_ipc_mem_handle, cudaIpcMemLazyEnablePeerAccess);
+      if (err != cudaSuccess) {
+        throw PythonBackendException(std::string(
+                                         "failed to open cuda ipc handle: " +
+                                         std::string(cudaGetErrorString(err)))
+                                         .c_str());
+      }
+      // Adjust the offset. cudaIpcOpenMemHandle will map the base address of a
+      // GPU pointer and the offset is not preserved when transferring the
+      // pointer using cudaIpcMemHandle.
+      data = data + raw_data->offset;
+      pb_tensor = std::make_shared<PbTensor>(
+          name, std::vector<int64_t>(dims, dims + dims_count),
+          tensor_shm->dtype, raw_data->memory_type, raw_data->memory_type_id,
+          data, raw_data->byte_size, nullptr /* DLManaged Tensor */);
+      pb_tensor->cuda_ipc_mem_handle_ = cuda_ipc_mem_handle;
+    } else {
+      pb_tensor = std::make_shared<PbTensor>(
+          name, std::vector<int64_t>(dims, dims + dims_count),
+          tensor_shm->dtype, raw_data->memory_type, raw_data->memory_type_id,
+          data, raw_data->byte_size, nullptr /* DLManaged Tensor */);
+      pb_tensor->reused_tensor_offset_ = raw_data->offset;
+      pb_tensor->cuda_ipc_mem_handle_ = cuda_ipc_mem_handle;
+      pb_tensor->is_reused_ = true;
+    }
+#endif // TRITON_ENABLE_GPU
+  }
+
+  return pb_tensor;
+}
+
+#ifdef TRITON_PB_STUB
+std::shared_ptr<PbTensor>
 PbTensor::FromDLPack(const std::string& name, const py::capsule& dlpack_tensor)
 {
   DLManagedTensor* dl_managed_tensor =
@@ -294,21 +416,35 @@ PbTensor::FromDLPack(const std::string& name, const py::capsule& dlpack_tensor)
       name, dims, dtype, memory_type, memory_type_id, memory_ptr, byte_size,
       dl_managed_tensor);
 }
+#endif // TRITON_PB_STUB
 
-PbTensor::~PbTensor()
+PbTensor::~PbTensor() noexcept(false)
 {
+#ifdef TRITON_ENABLE_GPU
+  if (!IsCPU() && cuda_ipc_mem_handle_ != nullptr &&
+      destruct_cuda_ipc_mem_handle_) {
+    cudaError_t err = cudaIpcCloseMemHandle(GetGPUStartAddress());
+    cuda_ipc_mem_handle_ = nullptr;
+    if (err != cudaSuccess) {
+      throw PythonBackendException(std::string(
+                                       "failed to close cuda ipc handle: " +
+                                       std::string(cudaGetErrorString(err)))
+                                       .c_str());
+    }
+  }
+#endif // TRITON_ENABLE_GPU
   DeleteDLPack();
 }
 
-
 const std::string&
-PbTensor::Name()
+PbTensor::Name() const
 {
   return name_;
 }
 
-py::array&
-PbTensor::AsNumpy()
+#ifdef TRITON_PB_STUB
+const py::array&
+PbTensor::AsNumpy() const
 {
   if (this->IsCPU()) {
     return numpy_array_;
@@ -319,15 +455,145 @@ PbTensor::AsNumpy()
 
   return numpy_array_;
 }
+#endif // TRITON_PB_STUB
+
+#ifdef TRITON_ENABLE_GPU
+void*
+PbTensor::GetGPUStartAddress()
+{
+  if (!this->IsCPU()) {
+    CUdeviceptr start_address;
+    CUresult cuda_err = cuPointerGetAttribute(
+        &start_address, CU_POINTER_ATTRIBUTE_RANGE_START_ADDR,
+        (CUdeviceptr)this->GetDataPtr());
+    if (cuda_err != CUDA_SUCCESS) {
+      const char* error_string;
+      cuGetErrorString(cuda_err, &error_string);
+      throw PythonBackendException(
+          std::string(
+              "failed to get cuda pointer device attribute: " +
+              std::string(error_string))
+              .c_str());
+    }
+
+    return reinterpret_cast<void*>(start_address);
+  }
+
+  throw PythonBackendException(
+      "Calling GetGPUStartAddress function on a CPU tensor.");
+}
+
+void
+PbTensor::SetReusedIpcHandle(cudaIpcMemHandle_t* cuda_ipc_mem_handle)
+{
+  destruct_cuda_ipc_mem_handle_ = false;
+  cuda_ipc_mem_handle_ = cuda_ipc_mem_handle;
+  is_reused_ = true;
+}
+
+cudaIpcMemHandle_t*
+PbTensor::CudaIpcMemHandle()
+{
+  return cuda_ipc_mem_handle_;
+}
+#endif // TRITON_ENABLE_GPU
+
+void
+PbTensor::SaveToSharedMemory(
+    std::unique_ptr<SharedMemory>& shm_pool, Tensor* tensor_shm, bool copy)
+{
+  const std::string& tensor_name = this->Name();
+  TRITONSERVER_DataType dtype_triton =
+      static_cast<TRITONSERVER_DataType>(this->TritonDtype());
+  tensor_shm->is_reused = false;
+  TRITONSERVER_MemoryType memory_type = TRITONSERVER_MEMORY_CPU;
+  int64_t memory_type_id = 0;
+
+  if (this->IsCPU()) {
+    size_t dims_count = dims_.size();
+    memory_type = TRITONSERVER_MEMORY_CPU;
+    memory_type_id = 0;
+
+    char* data_in_shm;
+    char* data_ptr;
+
+    data_ptr = static_cast<char*>(memory_ptr_);
+
+    uint64_t* ptr_offset;
+    SaveTensorToSharedMemory(
+        shm_pool, tensor_shm, data_in_shm, memory_type, memory_type_id,
+        byte_size_, tensor_name.c_str(), dims_.data(), dims_count, dtype_triton,
+        &ptr_offset);
+    *ptr_offset = 0;
+
+    // TODO: We can remove this memcpy if the numpy object
+    // is already in shared memory.
+    if (copy) {
+      std::copy(data_ptr, data_ptr + byte_size_, data_in_shm);
+    } else {
+      memory_ptr_ = reinterpret_cast<void*>(data_in_shm);
+    }
+  } else {
+#ifdef TRITON_ENABLE_GPU
+    char* cuda_handle;
+    uint64_t* ptr_offset;
+    SaveTensorToSharedMemory(
+        shm_pool, tensor_shm, cuda_handle, this->MemoryType(),
+        this->MemoryTypeId(), this->ByteSize(), tensor_name.c_str(),
+        this->Dims().data(), this->Dims().size(), dtype_triton, &ptr_offset);
+    char* d_ptr = reinterpret_cast<char*>(this->GetDataPtr());
+    *ptr_offset = GetDevicePointerOffset(d_ptr);
+    if (!IsReused()) {
+      cudaSetDevice(this->MemoryTypeId());
+      cudaError_t err = cudaIpcGetMemHandle(
+          reinterpret_cast<cudaIpcMemHandle_t*>(cuda_handle),
+          this->GetDataPtr());
+      if (err != cudaSuccess) {
+        throw PythonBackendException(std::string(
+                                         "failed to get cuda ipc handle: " +
+                                         std::string(cudaGetErrorString(err)))
+                                         .c_str());
+      }
+      cuda_ipc_mem_handle_ = reinterpret_cast<cudaIpcMemHandle_t*>(cuda_handle);
+    } else {
+      tensor_shm->is_reused = true;
+      RawData* raw_data;
+      shm_pool->MapOffset((char**)&raw_data, tensor_shm->raw_data);
+      raw_data->offset = *ptr_offset;
+      *(reinterpret_cast<cudaIpcMemHandle_t*>(cuda_handle)) =
+          *CudaIpcMemHandle();
+    }
+    void* start_address = this->GetGPUStartAddress();
+    *ptr_offset = reinterpret_cast<char*>(this->GetDataPtr()) -
+                  reinterpret_cast<char*>(start_address);
+#else
+    throw PythonBackendException(
+        "Python backend was not built with GPU tensor support");
+#endif // TRITON_ENABLE_GPU
+  }
+}
+
+void
+PbTensor::SetDataPtr(void* ptr)
+{
+  memory_ptr_ = reinterpret_cast<void*>(
+      (reinterpret_cast<char*>(ptr) + reused_tensor_offset_));
+}
+
+bool
+PbTensor::IsReused()
+{
+  return is_reused_;
+}
 
 int
-PbTensor::TritonDtype()
+PbTensor::TritonDtype() const
 {
   return dtype_;
 }
 
 void*
-PbTensor::GetDataPtr()
+PbTensor::GetDataPtr() const
 {
   return memory_ptr_;
 }
