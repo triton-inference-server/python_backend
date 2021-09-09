df54c536890d99e8d4de3b8814bb6c3c92b9b7b6
Iman Tabrizian <itabrizian@nvidia.com>
2021-09-09 17:21:16 -0400

    Make Python examples CI verifiable (#83)

--- examples/bls/async_client.py
+++ examples/bls/async_client.py
@@ -26,6 +26,7 @@
 
 from tritonclient.utils import *
 import tritonclient.http as httpclient
+import sys
 import numpy as np
 
 model_name = "bls_async"
@@ -40,6 +41,7 @@ with httpclient.InferenceServerClient("localhost:8000") as client:
         httpclient.InferInput("INPUT1", input1_data.shape,
                               np_to_triton_dtype(input1_data.dtype)),
     ]
+
     inputs[0].set_data_from_numpy(input0_data)
     inputs[1].set_data_from_numpy(input1_data)
 
@@ -54,7 +56,21 @@ with httpclient.InferenceServerClient("localhost:8000") as client:
                             outputs=outputs)
 
     result = response.get_response()
+    output0_data = response.as_numpy("OUTPUT0")
+    output1_data = response.as_numpy("OUTPUT1")
+
     print("INPUT0 ({}) + INPUT1 ({}) = OUTPUT0 ({})".format(
-        input0_data, input1_data, response.as_numpy("OUTPUT0")))
-    print("INPUT0 ({}) - INPUT1 ({}) = OUTPUT1 ({})".format(
-        input0_data, input1_data, response.as_numpy("OUTPUT1")))
+        input0_data, input1_data, output0_data))
+    print("INPUT0 ({}) - INPUT1 ({}) = OUTPUT0 ({})".format(
+        input0_data, input1_data, output1_data))
+
+    if not np.allclose(input0_data + input1_data, output0_data):
+        print("BLS async example error: incorrect sum")
+        sys.exit(1)
+
+    if not np.allclose(input0_data - input1_data, output1_data):
+        print("BLS async example error: incorrect difference")
+        sys.exit(1)
+
+    print('PASS: BLS Async')
+    sys.exit(0)
