e1fe868d2a211f8242b24544b8b9dae95ac4fed0
Katherine Yang <katheriney@nvidia.com>
2021-11-16 12:27:55 -0800

    modify setup script and qa folder to hold all test related files

--- inferentia/qa/setup_test_enviroment_and_test.sh
+++ inferentia/qa/setup_test_enviroment_and_test.sh
@@ -25,74 +25,88 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+export TRITON_SERVER_BRANCH_NAME=kyang-add-inferentia-test
 export TRITON_PATH="/home/ubuntu"
-cd ${TRITON_PATH}
-
-export PYTHON_BACKEND_BRANCH_NAME:=${PYTHON_BACKEND_BRANCH_NAME:="main"}
-export TRITON_SERVER_BRANCH_NAME:=${TRITON_SERVER_BRANCH_NAME:="main"}
-export TRITON_CLIENT_REPO_TAG:=${TRITON_CLIENT_REPO_TAG:="main"}
-export TRITON_CONTAINER_VERSION="21.10"
+export DEFAULT_REPO_TAG="main"
+export TRITON_COMMON_REPO_TAG=${DEFAULT_REPO_TAG}
+export TRITON_CORE_REPO_TAG=${DEFAULT_REPO_TAG}
+export TRITON_BACKEND_REPO_TAG=${DEFAULT_REPO_TAG}
+export TRITON_THIRD_PARTY_REPO_TAG=${DEFAULT_REPO_TAG}
+export IDENTITY_BACKEND_REPO_TAG=${DEFAULT_REPO_TAG}
+export PYTHON_BACKEND_REPO_TAG=${DEFAULT_REPO_TAG}
+export CHECKSUM_REPOAGENT_REPO_TAG=${DEFAULT_REPO_TAG}
+export TRITON_SERVER_BRANCH_NAME=${TRITON_SERVER_BRANCH_NAME:=${DEFAULT_REPO_TAG}}
+export TRITON_CLIENT_REPO_TAG=${TRITON_CLIENT_REPO_TAG:=${DEFAULT_REPO_TAG}}
+export TRITON_VERSION="2.17.0dev"
+export TRITON_CONTAINER_VERSION="21.12dev"
+export TRITON_UPSTREAM_CONTAINER_VERSION="21.10"
 export BASE_IMAGE=tritonserver
 export SDK_IMAGE=tritonserver_sdk
 export BUILD_IMAGE=tritonserver_build
 export QA_IMAGE=tritonserver_qa
 
-
-export TEST_JSON_REPO=${INFERENTIA_PATH}/server/qa/common/inferentia_perf_analyzer_input_data_json
-export TEST_REPO=${INFERENTIA_PATH}/server/qa/L0_inferentia_perf_analyzer
+export TEST_JSON_REPO=${TRITON_PATH}/server/qa/common/inferentia_perf_analyzer_input_data_json
+export TEST_REPO=${TRITON_PATH}/server/qa/L0_inferentia_perf_analyzer
 export TEST_SCRIPT="test.sh"
 
+cd ${TRITON_PATH}
 # Clone necessary branches
-git clone --single-branch --depth=1 -b ${TRITON_SERVER_BRANCH_NAME}
-          https://github.com/triton-inference-server/python_backend.git
-git clone --single-branch --depth=1 -b ${TRITON_SERVER_BRANCH_NAME}
+rm -rf ${TRITON_PATH}/server
+git clone --single-branch --depth=1 -b ${TRITON_SERVER_BRANCH_NAME} \
           https://github.com/triton-inference-server/server.git
 echo ${TRITON_VERSION} > server/TRITON_VERSION
-cd ${INFERNTIA_PATH}/server
-git clone --single-branch --depth=1 -b ${TRITON_CLIENT_REPO_TAG}
+cd ${TRITON_PATH}/server
+git clone --single-branch --depth=1 -b ${TRITON_CLIENT_REPO_TAG} \
           https://github.com/triton-inference-server/client.git clientrepo
 
 # First set up inferentia and run in detatched mode
 cd ${TRITON_PATH}/python_backend
 chmod 777 ${TRITON_PATH}/python_backend/inferentia/scripts/setup-pre-container.sh
 sudo ${TRITON_PATH}/python_backend/inferentia/scripts/setup-pre-container.sh
-
+       #     --image=base,"nvcr.io/nvidia/tritonserver:${TRITON_UPSTREAM_CONTAINER_VERSION}-py3" \
 # Build container with only python backend 
-cd ${TRITON_PATH}
+cd ${TRITON_PATH}/server
+pip3 install docker
 ./build.py --build-dir=/tmp/tritonbuild \
            --cmake-dir=${TRITON_PATH}/server/build \
+           --version=${TRITON_VERSION} \
+           --container-version=${TRITON_CONTAINER_VERSION} \
            --enable-logging --enable-stats --enable-tracing \
            --enable-metrics --enable-gpu-metrics --enable-gpu \
            --filesystem=gcs --filesystem=azure_storage --filesystem=s3 \
            --endpoint=http --endpoint=grpc \
-           --image=base,nvcr.io/nvidia/tritonserver:${TRITON_CONTAINER_VERSION}-py3 \
-           --repo-tag=common:main \ 
-           --repo-tag=core:main \
-           --repo-tag=backend:main \
-           --repo-tag=thirdparty:main \
-           --backend=identity:main \
-           --backend=python:main \
-           --repoagent=checksum:main
+           --repo-tag=common:${TRITON_COMMON_REPO_TAG} \
+           --repo-tag=core:${TRITON_CORE_REPO_TAG} \
+           --repo-tag=backend:${TRITON_BACKEND_REPO_TAG} \
+           --repo-tag=thirdparty:${TRITON_THIRD_PARTY_REPO_TAG} \
+           --backend=identity:${IDENTITY_BACKEND_REPO_TAG} \
+           --backend=python:${PYTHON_BACKEND_REPO_TAG} \
+           --repoagent=checksum:${CHECKSUM_REPOAGENT_REPO_TAG}
 docker tag tritonserver_build "${BUILD_IMAGE}"
 docker tag tritonserver "${BASE_IMAGE}"
 
 # Build docker container for SDK
 docker build -t ${SDK_IMAGE} \
-             -f ${TRITON_PATH}/Dockerfile.sdk \
+             -f ${TRITON_PATH}/server/Dockerfile.sdk \
              --build-arg "TRITON_CLIENT_REPO_SUBDIR=clientrepo" .
 
 # Build QA container
-docker build -t ${QA_IMAGE}
-                   -f ${TRITON_PATH}/Dockerfile.QA 
-                   --build-arg "TRITON_PATH=${TRITON_PATH}"
-                   --build-arg "SDK_IMAGE=${SDK_IMAGE}"
-                   --build-arg "BASE_IMAGE=${BASE_IMAGE}" .
+docker build -t ${QA_IMAGE} \
+                   -f ${TRITON_PATH}/python_backend/inferentia/qa/Dockerfile.QA \
+                   --build-arg "TRITON_PATH=${TRITON_PATH}" \
+                   --build-arg "BASE_IMAGE=${BASE_IMAGE}"   \
+                   --build-arg "BUILD_IMAGE=${BUILD_IMAGE}" \
+                   --build-arg "SDK_IMAGE=${SDK_IMAGE}"     .
 
 # log into the docker
-docker run -v /home/ubuntu:$TRITON_PATH \
-           -e TEST_REPO=${TEST_REPO} \
-           -e TEST_JSON_REPO=${TEST_JSON_REPO} \
-           --net host -ti ${QA_IMAGE}  \
-           /bin/bash -c "bash -ex ${TEST_REPO}/${TEST_SCRIPT}"
-
-
+docker run --device /dev/neuron0                   \
+            -v /home/ubuntu:$TRITON_PATH           \
+            -v /lib/udev:/mylib/udev               \
+            --shm-size=1g --ulimit memlock=-1      \
+            -p 8000:8000 -p 8001:8001 -p 8002:8002 \
+            --ulimit stack=67108864                \
+            -e TEST_REPO=${TEST_REPO}              \
+            -e TEST_JSON_REPO=${TEST_JSON_REPO}    \
+            -e TRITON_PATH=${TRITON_PATH}          \
+            --net host -ti ${QA_IMAGE}             \
+            /bin/bash -c "bash cd ${TEST_REPO} && bash -ex ${TEST_REPO}/${TEST_SCRIPT}"
