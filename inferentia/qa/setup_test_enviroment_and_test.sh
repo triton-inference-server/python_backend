7c3cefef78f5228c61bc52f31113506024e46540
Katherine Yang <katheriney@nvidia.com>
2021-11-23 12:51:47 -0800

    add tensorflow tests

--- inferentia/qa/setup_test_enviroment_and_test.sh
+++ inferentia/qa/setup_test_enviroment_and_test.sh
@@ -97,7 +97,7 @@ export TEST_JSON_REPO=/opt/tritonserver/qa/common/inferentia_perf_analyzer_input
 export TEST_REPO=/opt/tritonserver/qa/L0_inferentia_perf_analyzer
 export TEST_SCRIPT="test.sh"
 
-# Run single instance test
+# Run pytorch instance test
 CONTAINER_NAME="qa_container"
 docker stop ${CONTAINER_NAME} && docker rm ${CONTAINER_NAME}
 docker create --name ${CONTAINER_NAME}             \
@@ -109,6 +109,26 @@ docker create --name ${CONTAINER_NAME}             \
             -e TEST_REPO=${TEST_REPO}              \
             -e TEST_JSON_REPO=${TEST_JSON_REPO}    \
             -e TRITON_PATH=${TRITON_PATH}          \
+            -e USE_TENSORFLOW="0"                  \
+            --net host -ti ${QA_IMAGE}             \
+            /bin/bash -c "bash -ex ${TEST_REPO}/${TEST_SCRIPT}" && \
+            docker cp /lib/udev ${CONTAINER_NAME}:/mylib/udev && \
+            docker cp /home/ubuntu/python_backend ${CONTAINER_NAME}:${TRITON_PATH}/python_backend && \
+            docker start -a ${CONTAINER_NAME} || RV=$?;
+
+
+# Run tensorflow instance test
+docker stop ${CONTAINER_NAME} && docker rm ${CONTAINER_NAME}
+docker create --name ${CONTAINER_NAME}             \
+            --device /dev/neuron0                  \
+            --device /dev/neuron1                  \
+            --shm-size=1g --ulimit memlock=-1      \
+            -p 8000:8000 -p 8001:8001 -p 8002:8002 \
+            --ulimit stack=67108864                \
+            -e TEST_REPO=${TEST_REPO}              \
+            -e TEST_JSON_REPO=${TEST_JSON_REPO}    \
+            -e TRITON_PATH=${TRITON_PATH}          \
+            -e USE_TENSORFLOW="1"                  \
             --net host -ti ${QA_IMAGE}             \
             /bin/bash -c "bash -ex ${TEST_REPO}/${TEST_SCRIPT}" && \
             docker cp /lib/udev ${CONTAINER_NAME}:/mylib/udev && \
