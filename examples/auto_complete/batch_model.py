f17fefe6be6c35ec903455c57a1e14807ead0506
Tanmay Verma <tanmay2592@gmail.com>
2022-07-13 14:44:05 -0700

    Update docs in accordance with the new auto-complete-config behavior (#174)
    * Update docs in accordance with the new auto-complete-config behavior
    
    * Address the review comments to fix the language

--- examples/auto_complete/batch_model.py
+++ examples/auto_complete/batch_model.py
@@ -40,19 +40,17 @@ class TritonPythonModel:
 
     @staticmethod
     def auto_complete_config(auto_complete_model_config):
-        """`auto_complete_config` is called only once when the server is
-        started with `--strict-model-config=false`. Implementing this
-        function is optional. A no implementation of `auto_complete_config`
-        will do nothing. This function can be used to set `max_batch_size`,
-        `input` and `output` properties of the model using
-        `set_max_batch_size`, `add_input`, and `add_output`. These properties
-        will allow Triton to load the model with minimal model configuration
-        in absence of a configuration file. This function returns the
-        `pb_utils.ModelConfig` object with these properties. You can use
-        `as_dict` function to gain read-only access to the
-        `pb_utils.ModelConfig` object. The `pb_utils.ModelConfig` object being
-        returned from here will be used as the final configuration for the
-        model.
+        """`auto_complete_config` is called only once when loading the model assuming
+        the server was not started with `--disable-auto-complete-config`. Implementing
+        this function is optional. No implementation of `auto_complete_config` will
+        do nothing. This function can be used to set `max_batch_size`, `input` and
+        `output` properties of the model using `set_max_batch_size`, `add_input`, and
+        `add_output`. These properties will allow Triton to load the model with minimal
+        model configuration in absence of a configuration file. This function returns
+        the `pb_utils.ModelConfig` object with these properties. You can use the `as_dict`
+        function to gain read-only access to the `pb_utils.ModelConfig` object.
+        The `pb_utils.ModelConfig` object being returned from here will be used as
+        the final configuration for the model.
 
         Note: The Python interpreter used to invoke this function will be
         destroyed upon returning from this function and as a result none of
