9de633a4d27e8dbfd0ed402772585ea8cbd66ffa
kthui <18255193+kthui@users.noreply.github.com>
2023-01-26 11:42:37 -0800

    Atomically increase/read stub process count and fix random boost truncate fail (#205)
    * Atomically increase and read stub process count
    
    * Allow retry if truncate fail
    
    * Remove retry and remove shm region before create
    
    * Upgrade boost to 1.79.0

--- src/shm_manager.h
+++ src/shm_manager.h
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// Copyright 2021-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
@@ -35,6 +35,7 @@
 #include <type_traits>
 #include <typeinfo>
 #include <vector>
+#include <functional>
 #include "pb_exception.h"
 
 namespace triton { namespace backend { namespace python {
