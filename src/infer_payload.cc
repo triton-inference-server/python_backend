4c59652f303712893d45e32c0d7008df845de051
Iman Tabrizian <itabrizian@nvidia.com>
2023-05-15 16:34:47 -0400

    Serialize all the responses in decoupled BLS

--- src/infer_payload.cc
+++ src/infer_payload.cc
@@ -41,15 +41,13 @@ InferPayload::SetValue(std::unique_ptr<InferResponse> infer_response)
 {
   // Only set value to the promise with the first response. Call the callback
   // function to send decoupled response to the stub.
-  {
-    std::lock_guard<std::mutex> lock(mutex_);
-    if (!is_promise_set_) {
-      is_promise_set_ = true;
-      promise_->set_value(std::move(infer_response));
-      return;
-    }
+  std::lock_guard<std::mutex> lock(mutex_);
+  if (!is_promise_set_) {
+    is_promise_set_ = true;
+    promise_->set_value(std::move(infer_response));
+  } else {
+    Callback(std::move(infer_response));
   }
-  Callback(std::move(infer_response));
 }
 
 void
