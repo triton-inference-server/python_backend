3e834522744adee73a8594880bd9b3cc1fb95e12
tanmayv25 <tanmay2592@gmail.com>
2023-06-27 15:59:08 -0700

    rebase

--- src/shm_manager.h
+++ src/shm_manager.h
@@ -37,10 +37,6 @@
 #include <type_traits>
 #include <typeinfo>
 #include <vector>
-<<<<<<< HEAD
-
-=======
->>>>>>> Interfacing with plugin models
 #include "pb_exception.h"
 
 namespace triton { namespace backend { namespace python {
