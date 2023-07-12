7158473ec6554f0aa62a537244c51299a1d523b5
Neelay Shah <neelays@nvidia.com>
2023-07-12 12:43:32 -0700

    Update src/resources/triton_python_backend_utils.py
    updated to be explicit in choice to use native byte order

--- src/resources/triton_python_backend_utils.py
+++ src/resources/triton_python_backend_utils.py
@@ -114,7 +114,7 @@ def deserialize_bytes_tensor(encoded_tensor):
     while offset < len(val_buf):
         l = struct.unpack_from("@I", val_buf, offset)[0]
         offset += 4
-        sb = struct.unpack_from("{}s".format(l), val_buf, offset)[0]
+        sb = struct.unpack_from("@{}s".format(l), val_buf, offset)[0]
         offset += l
         strs.append(sb)
     return (np.array(strs, dtype=np.object_))
