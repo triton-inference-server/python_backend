7cb2ef096514e39aa7add421f37633f5a524f30d
Tanmay Verma <tanmay2592@gmail.com>
2023-08-15 13:11:54 -0700

    Add PyTorch platform handler example (#287) (#290)
    * Add PyTorch platform handler example
    
    * Refactor docs structure
    
    * Add more comments and minor refactoring
    
    * Further break down client.py
    
    * Remove exit 0 if terminated normally
    
    * Simplify comments
    
    * Improve comment
    
    * List mug.jpg paths
    
    * Docs update
    
    * Describe the source of mug.jpg
    
    Co-authored-by: Jacky <18255193+kthui@users.noreply.github.com>

---
+++ examples/pytorch_platform_handler/model.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+# Copyright 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import torch
+
+
+class ResNet50(torch.nn.Module):
+    # This is a native PyTorch model class. `TritonPythonModel` is not needed.
+
+    def __init__(self):
+        super().__init__()
+        self._model = torch.hub.load(
+            "pytorch/vision",
+            "resnet50",
+            weights="ResNet50_Weights.IMAGENET1K_V2",
+            skip_validation=True,
+        )
+
+    def forward(self, input_tensor):
+        output_tensor = self._model(input_tensor)
+        return output_tensor
