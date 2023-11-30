34915beee3bf1f843f3d3b0dc4c7e59af055ea1d
fpetrini15 <francescogpetrini@gmail.com>
2023-11-30 10:20:46 -0800

    Unecessary assignment

--- src/python_be.cc
+++ src/python_be.cc
@@ -372,7 +372,7 @@ ModelInstanceState::SaveRequestsToSharedMemory(
     }
     InferenceTrace trace = InferenceTrace(triton_trace);
 
-    uint64_t request_timeout = 0;
+    uint64_t request_timeout;
     RETURN_IF_ERROR(TRITONBACKEND_InferenceRequestTimeoutMicroseconds(
         request, &request_timeout));
 
