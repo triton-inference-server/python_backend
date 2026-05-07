04d1e45c9e26d7e0f8594be73fd917421b172f43
Denis Kolesnikov <deniskolesnikov358@gmail.com>
2026-05-07 20:21:55 +0300

    almostly finished fixing envs overfitting

--- src/stub_launcher.cc
+++ src/stub_launcher.cc
@@ -64,7 +64,7 @@ StubLauncher::Initialize(ModelState* model_state)
   shm_growth_byte_size_ = model_state->StateForBackend()->shm_growth_byte_size;
   shm_message_queue_size_ =
       model_state->StateForBackend()->shm_message_queue_size;
-  python_execution_env_ = model_state->PythonExecutionEnv();
+  python_execution_env_source_ = model_state->PythonExecutionEnv();
   python_lib_ = model_state->StateForBackend()->python_lib;
   model_state->ModelConfig().Write(&model_config_buffer_);
   is_decoupled_ = model_state->IsDecoupled();
@@ -104,7 +104,7 @@ StubLauncher::Initialize(ModelState* model_state)
 
   // FIXME [DLIS-5969]: Enable for Windows when custom execution environments
   // are supported.
-  if (python_execution_env_ != "") {
+  if (python_execution_env_source_ != "") {
 #ifndef _WIN32
     RETURN_IF_ERROR(GetPythonEnvironment(model_state));
 #else
@@ -405,7 +405,7 @@ StubLauncher::Launch()
   // executables and libraries.
   ipc_control_->uses_env = false;
 
-  if (python_execution_env_ != "") {
+  if (python_execution_env_source_ != "") {
     ipc_control_->uses_env = true;
 
     // Parse environment variables from activation script
@@ -592,20 +592,18 @@ StubLauncher::Launch()
 TRITONSERVER_Error*
 StubLauncher::GetPythonEnvironment(ModelState* model_state)
 {
-  std::string python_execution_env = "";
   try {
-    python_execution_env =
-        model_state->StateForBackend()->env_manager->ExtractIfNotExtracted(
-            python_execution_env_);
+    python_execution_env_ =
+        model_state->StateForBackend()->env_manager->GetEnvironment(model_state);
   }
   catch (PythonBackendException& pb_exception) {
     return TRITONSERVER_ErrorNew(
         TRITONSERVER_ERROR_INTERNAL, pb_exception.what());
   }
 
-  path_to_activate_ = python_execution_env + "/bin/activate";
-  path_to_libpython_ = python_execution_env + "/lib";
-  if (python_execution_env.length() > 0 && !FileExists(path_to_activate_)) {
+  path_to_activate_ = python_execution_env_->Path() + "/bin/activate";
+  path_to_libpython_ = python_execution_env_->Path() + "/lib";
+  if (python_execution_env_->Path().length() > 0 && !FileExists(path_to_activate_)) {
     return TRITONSERVER_ErrorNew(
         TRITONSERVER_ERROR_INTERNAL,
         ("Path " + path_to_activate_ +
